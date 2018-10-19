f861afb92e9d059c7f5f0aa2cdadb0a8bcf41933
AdityaMunot <44190316+AdityaMunot@users.noreply.github.com>
2018-10-19 00:43:43 -0400

    Update Simple-Anagram.py
    Added Unit test Cases

--- Simple Anagram.py
+++ Simple Anagram.py
@@ -1,3 +1,16 @@
+# implement using only only string and list.
+
 def anagram(st1, st2):
 
     return sorted(st1) == sorted(st2)
+
+class test(unittest.TestCase):
+
+    def test_anagram(self):
+
+        self.assertTrue(anagram('iceman', 'cinema'))
+        self.assertFalse(anagram('pikapika', 'pikapik'), False)
+        
+if __name__ == '__main__':
+    # note: there is no main(). Only test cases here
+    unittest.main(exit=False, verbosity=2)
